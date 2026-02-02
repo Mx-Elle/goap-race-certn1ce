d4276f7381bd3843c5def748e846c2ca8d6e439d
Niko <100811788+certn1ce@users.noreply.github.com>
2026-02-02 14:29:33 -0500

    added bare bones

---
+++ best_bot.py
@@ -0,0 +1,11 @@
+from game_world.racetrack import RaceTrack
+
+Point = tuple[int, int]
+
+
+
+def astar(location: Point, target: Point, map: RaceTrack) -> list[tuple[int, int]]:
+    return []
+
+def best_move(location: Point, map: RaceTrack) -> Point:
+    return (0, 0)
