3d7d7aa99ee397efa1e3bd2cf82a01ff0aa5c359
Niko <100811788+certn1ce@users.noreply.github.com>
2026-02-17 11:45:15 -0500

    a little cleaning

--- best_bot.py
+++ best_bot.py
@@ -20,7 +20,6 @@ class best_bot:
 
     def __call__(self, location: Point, map: RaceTrack) -> Point:
         return self.best_move(location, map)
-        # return self.test_move(location, map)
 
 
     def dist(self, p1: Point, p2: Point) -> int:
@@ -74,61 +73,6 @@ class best_bot:
         self,
         starting_point: Point,
         target: Point,
-        map: RaceTrack,
-    ) -> deque[Point]:
-        """
-        finds shortest path between two points on the map
-
-        :return: sequence of cells from starting point to ending point
-        :rtype: list[Point]
-        """
-
-        frontier = list()
-        camefrom = dict()
-        path = deque()
-        found = False
-
-        # add first cell to frontier
-        # tuple is f-score, g-score, cell:point
-        heapq.heappush(frontier, (self.dist(starting_point, target), 0, starting_point))
-        camefrom[starting_point] = None
-
-        # expand frontier until all cells explored or shortest path found
-        while len(frontier) > 0:
-            # unpack tuple
-            (current_f, current_g, current_position) = frontier[0]
-
-            if current_position == target:
-                found = True
-                break
-
-            heapq.heappop(frontier)
-
-            for loc in self.tvrs_neighbors(current_position, map):
-                if loc not in camefrom:
-                    camefrom[loc] = current_position
-                    heapq.heappush(
-                        frontier,
-                        (self.dist(loc, target) + current_g + 1, current_g + 1, loc),
-                    )
-
-        # return empty path if no possible path to target
-        if found == False:
-            return deque()
-
-        # creating path
-        current_position = target
-        while current_position != starting_point:
-            path.appendleft(current_position)
-            current_position = camefrom[current_position]
-        path.appendleft(starting_point)
-
-        return path
-
-    def astar2(
-        self,
-        starting_point: Point,
-        target: Point,
         starting_map: RaceTrack,
     ) -> deque[Point]:
         """
@@ -147,7 +91,7 @@ class best_bot:
         tiebreaker: int = 0
 
         # add first cell to frontier
-        # tuple is f-score, g-score, cell:point, map at that point
+        # tuple is f-score, g-score, cell:point, tiebreaker, map at that point
         s_map_bytes = self.searialize(starting_map)
         camefrom[(starting_point, s_map_bytes)] = (None, None)
         g_scores[(starting_point, s_map_bytes)] = 0
@@ -198,8 +142,8 @@ class best_bot:
             return deque()
 
         # creating path
-        current_position = target  # should already be the case but just in case
-        current_map = end_state  # should already be the case but just in case
+        current_position = target
+        current_map = end_state  # current_map is now a serialized map not map object
         while (current_position, current_map) != (None, None):
             path.appendleft(current_position)
             (
@@ -213,22 +157,13 @@ class best_bot:
         if self.map_target == None:
             self.map_target = map.target
 
-            # self.current_path = self.astar(
-            #     location,
-            #     self.map_target,
-            #     map,
-            #     True
-            # )
-
-            self.current_path = self.astar2(
+            self.current_path = self.astar(
                 location,
                 self.map_target,
                 map,
             )
 
         if len(self.current_path) > 0:
-            vec = self.getVector(location, self.current_path.popleft())
-            return vec
-            # return self.getVector(location, self.current_path.popleft())
+            return self.getVector(location, self.current_path.popleft())
         else:
             raise ZeroDivisionError("no move")
